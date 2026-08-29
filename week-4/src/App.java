b27f983b6be2c2434e2f930b5b1689fff2088d74
Aditya <aditya0dagar@gmail.com>
2026-08-29 10:05:29 +0530

    Bank Account

--- week-4/src/App.java
+++ week-4/src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         StudentRecord SR = new StudentRecord();
         SR.StudentRecord_main();
+
+        BankAccount BA = new BankAccount();
+        BA.BankAccount_main();
     }
 }
