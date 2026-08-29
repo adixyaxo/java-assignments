c6b0a9a8847f09b23881722957bf61f3519d2b2c
Aditya <aditya0dagar@gmail.com>
2026-08-29 09:54:24 +0530

    Student Record

--- week-4/src/App.java
+++ week-4/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        StudentRecord SR = new StudentRecord();
+        SR.StudentRecord_main();
     }
 }
